1f347880419298d0737367a38a0b86f14b8d131a
manuel dellagala <manuel.dellagala@gmail.com>
2022-03-15 21:03:49 +0100

    funzioni email

--- src/AfricanBikers/Archive/ImportXls.php
+++ src/AfricanBikers/Archive/ImportXls.php
@@ -1,57 +1,112 @@
 <?php namespace AfricanBikers\Archive;
 
+
 use PDO;
+
+
 /**
- *  @param String $driver type of DB
+ * ImportXls
+ * @author Manuel della Gala
+ * 
  */
+class ImportXls extends PDO {
 
-class ImportXls extends PDO{
+    // TODO mettere in config
+    private $driver = 'mysql';
+    private $host = 'localhost';
+    private $port = '3306';
+    private $user = 'root';
+    private $pass = 'root';
+    // TODO end
 
-    private $driver = '';
-    private $host = '';
-    private $port = '';
-    private $user = '';
-    private $pass = '';
     private $link = NULL;
-    private $query = NULL;
-
-    function __construct($driver,$host,$port,$user,$pass){
-
-        $this->driver = $driver;
-        $this->host = $host;
-        $this->port = $port;
-        $this->user = $user;
-        $this->pass = $pass;
-
+    private $query = '';
+    public $conn = NULL;
+    
         
-    }
+    /**
+     * __construct
+     *
+     * @param  string $driver
+     * @param  string $host
+     * @param  int $port
+     * @param  string $user
+     * @param  string $pass
+     * @return void
+     */
+    function __construct(){
 
-    public function connect(){
         $conn = new PDO("$this->driver:$this->host;port=$this->port",$this->user,$this->pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
+        $this->conn = $conn;
+                
+    }
 
+    
+    public function createTable(string $table,$data){
+        $sql = "CREATE TABLE IF NOT EXISTS $table(
+            id BIGINT NOT NULL AUTO_INCREMENT ,";
+
+        foreach($data as $key=>$value){
+            
+            $sql .= " $key $value,";
+
+        }
+        //CREATE TABLE `africanbikers`.`xxx` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(10) NULL , `cognome` VARCHAR(10) NULL , `importo` FLOAT NULL , PRIMARY KEY (`id`)); 
+        $sql .= " PRIMARY KEY (`id`));";
+        echo($sql);
+        $this->conn->query($sql);
+
+        /*
+         * CREATE TABLE `africanbikers`.`xxx` ( `id` BIGINT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(10) NULL , `cognome` VARCHAR(10) NULL , `importo` FLOAT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
+         */
+            
+            
     }
 
     public function question($query){
-        $this->$query = $query;
 
-        return $this->connect()
-                    ->prepare($query)
-                    ->execute();
+        $this->conn->query($query); 
+
+        //$this->query ='';
+
+        return $this->conn;
                                      
 
     }
 
-    public function save(){
+    public function where(String $argA, String $argB, String $comparison="="):object{
+        
+        $this->query.=" WHERE $argA $comparison $argB ";
 
-                                
+        return $this;                                     
 
     }
 
+    public function insert(string $table, array $arrData){
+
+        $sqlFirst = "INSERT INTO donatori(";
+        $sqlSecond = "VALUE(";
+        foreach($arrData as $key => $value){
+
+            $sqlFirst .= " $key,";
+            $sqlSecond .= "'$value',";
+
+        }
+
+        $sqlFirst = substr($sqlFirst, 0, -1);  
+        $sqlSecond = substr($sqlSecond, 0, -1);      
+        
+        
+            echo ("* $sqlFirst) $sqlSecond);*");
+            $this->question(" $sqlFirst) $sqlSecond);");
+    }
+
+
+
     public function all(){
 
-        $this->connect()
+        $this->conn
             ->prepare("SELECT *;")
             ->execute();           
 
--- src/index.php
+++ src/index.php
@@ -35,29 +35,105 @@ $html = <<<HTML
 <div> 
    
 </div>
-    
-
 HTML;
 
+$desc = $cellValue[3];
+$name = $cellValue[0];
+$surname = $cellValue[1];
+$import = $cellValue[2];
 
+//TODO mettere in setup
+/*
 try {
-    $import = new ImportXls('mysql','localhost',3306,'root','root');
-       
-    $import->question('CREATE DATABASE IF NOT EXISTS africanbikers');
-    $import->save();
-            
-    echo "Database created successfully<br>";
+    $import = new ImportXls();
+    
+    $import->question('CREATE DATABASE IF NOT EXISTS africanbikers;');
+    $import->conn = null;
+    //echo "Database created";
   } catch(PDOException $e) {
     echo  "<br>" . $e->getMessage();
   }
   
   $conn = null;
 
+  try {
+    $import = new ImportXls();
+    $link = $import->conn;
+    //$link->question('CREATE DATABASE IF NOT EXISTS africanbikers;');
+
+    $import->question('USE africanbikers;');
+    $import->createTable("donatori",["nome" => "CHAR(20)","cognome"=>"CHAR(20)","importo"=>"FLOAT","descrizione"=>"VARCHAR(255)"]);
+       
+    $link = null;
+
+    echo "Table created";
+  } catch(PDOException $e) {
+    echo  "<br>" . $e->getMessage();
+  }
+
+  try {
+    $import = new ImportXls();
+    $link = $import->conn;
+    //$link->question('CREATE DATABASE IF NOT EXISTS africanbikers;');
+
+    $import->question('USE africanbikers;');
+    $import->insert("donatori",["nome" => "Manuel","cognome"=>"della Gala","importo"=>1000,"descrizione"=>"sdafsadfnsalkndkanfdsad"]);
+       
+    $link = null;
+
+    echo "Ok";
+    
+  } catch(PDOException $e) {
+    echo  "<br>" . $e->getMessage();
+  }
+  */
+  
+  // TODO end
+
+//include './AfricanBikers/Pdf/Config.php';
+
+error_reporting(E_ALL); // Genera un boundary 
+
+var_dump($success = mail('manuel.dellagala@gmail.com', 'My Subject', "test"));
+
+$mail_boundary = "=_NextPart_" . md5(uniqid(time())); 
+$to = "manueldg@tiscali.it"; 
+$subject = "Testing e-mail"; 
+$sender = "manuel.dellagala@gmail.com"; 
 
 
+$headers = "From: $sender\n"; 
+$headers .= "MIME-Version: 1.0\n"; 
+$headers .= "Content-Type: multipart/alternative;\n\tboundary=\"$mail_boundary\"\n";
+$headers .= "X-Mailer: PHP " . phpversion(); // Corpi del messaggio nei due formati testo e HTML 
 
-
-include './AfricanBikers/Pdf/Config.php';
+$text_msg = "$name $surname $import $desc"; 
+$html_msg = "<b>messaggio</b> in formato <p><a href='http://www.aruba.it'>html</a><br><img src=\"http://hosting.aruba.it/image_top/top_01.gif\" border=\"0\">$name $surname $import $desc</p>";
+  // Costruisci il corpo del messaggio da inviare 
+  $msg = "This is a multi-part message in MIME format.\n\n"; 
+  $msg .= "--$mail_boundary\n"; 
+  $msg .= "Content-Type: text/plain; charset=\"iso-8859-1\"\n"; 
+  $msg .= "Content-Transfer-Encoding: 8bit\n\n"; 
+  $msg .= "Questa è una e-Mail di test inviata dal servizio Hosting di Aruba.it per la verifica del corretto funzionamento di PHP mail()function. Aruba.it";
+   // aggiungi il messaggio in formato text 
+   $msg .= "\n--$mail_boundary\n"; 
+   $msg .= "Content-Type: text/html; charset=\"iso-8859-1\"\n"; 
+   $msg .= "Content-Transfer-Encoding: 8bit\n\n"; 
+   $msg .= "Questa è una e-Mail di test inviata dal servizio Hosting di Aruba.it per la verifica del corretto funzionamento di PHP mail()function. Aruba.it";
+    // aggiungi il messaggio in formato HTML 
+    // Boundary di terminazione multipart/alternative 
+    $msg .= "\n--$mail_boundary--\n"; 
+    // Imposta il Return-Path (funziona solo su hosting Windows) 
+    ini_set("sendmail_from", $sender); 
+    // Invia il messaggio, il quinto parametro "-f$sender" imposta il Return-Path su hosting Linux 
+    if (mail($to, $subject, $msg, $headers, "-f$sender")) { 
+      echo "Mail inviata correttamente!<br><br>Questo di seguito è il codice sorgente usato per l'invio della mail:<br><br>"; 
+      highlight_file($_SERVER["SCRIPT_FILENAME"]); 
+      unlink($_SERVER["SCRIPT_FILENAME"]); 
+    } 
+    else { 
+      echo "<br><br>Recapito e-Mail fallito!"; 
+    } 
 
 
 
